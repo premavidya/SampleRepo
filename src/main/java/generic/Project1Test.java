26679871b17b651d6b0e2e2f4c61078fec9c0dad
premavidya <mpremavidya28@gmail.com>
2025-08-28 15:43:13 +0530

    commit1

---
+++ src/main/java/generic/Project1Test.java
@@ -0,0 +1,15 @@
+package generic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Project1Test {
+
+	@Test
+	public void login()
+	{
+		System.out.println("Login");
+	}
+}
---
+++ src/test/java/demoProject/SampleTest.java
@@ -0,0 +1,15 @@
+package demoProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void launch()
+	{
+		System.out.println("Launch");
+	}
+}
